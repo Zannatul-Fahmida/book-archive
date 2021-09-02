9c5722c1b4f544b7f2d39e82b9eca726852ffbdf
Zannatul-Fahmida <jannatulfahmida15@gmail.com>
2021-09-02 15:04:54 +0600

    done

--- js/main.js
+++ js/main.js
@@ -3,32 +3,42 @@ const searchBooks = () => {
     const searchText = searchField.value;
     //Clear search field data
     searchField.value = '';
-    if (searchText == '') {
-        alert('Please give a text to search');
+    if (searchText === '') {
+        alert('Please give a book name to search');
     }
     else {
         const url = ` http://openlibrary.org/search.json?q=${searchText}`;
         fetch(url)
             .then(res => res.json())
-            .then(data => displayBooks(data.docs))
+            .then(data => displayBooks(data))
     }
 }
 const displayBooks = books => {
     const searchResult = document.getElementById('search-result');
-    books.forEach(book => {
-        console.log(book);
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
-        <div class="card h-100">
-            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" class="card-img-top" alt="...">
-            <div class="card-body">
-              <h5 class="card-title">${book.title}</h5>
-              <p class="card-text">${book.author_name[0]}</p>
-              <p class="card-text">${book.first_publish_year}</p>
+    searchResult.textContent = '';
+    const searchFoundResult = document.getElementById('search-found');
+    searchFoundResult.textContent = '';
+    const h3 = document.createElement('h3');
+    searchFoundResult.appendChild(h3);
+    if (books.numFound === 0) {
+        h3.innerText = 'No result found';
+    }
+    else {
+        h3.innerText = `Search found: ${books.numFound}`;
+    }
+    books.docs.forEach(book => {
+            const div = document.createElement('div');
+            div.classList.add('col');
+            div.innerHTML = `
+            <div class="card h-100">
+                <img src="https://covers.openlibrary.org/b/id/${book.cover_i ? book.cover_i : ''}-M.jpg" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title">${book.title}</h5>
+                    <p class="card-text">${book.author_name[0] ? book.author_name[0] : 'unknown'}</p>
+                    <p class="card-text">${book.first_publish_year ? book.first_publish_year : ''}</p>
+                </div>
             </div>
-        </div>
-        `;
-        searchResult.appendChild(div);
+            `;
+            searchResult.appendChild(div);
     })
 }
